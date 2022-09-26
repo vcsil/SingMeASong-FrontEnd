bac4ca894684ac3de0934fb16e37c1e3c52a006e
vcsil <vcsilva@poli.ufrj.br>
2022-09-26 19:38:02 -0300

    chore: ignore errors eslint

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
     },
-  },
 });
